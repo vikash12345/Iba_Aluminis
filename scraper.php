ce95e7e522400934fa92e09e69ff3dcc05811231
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-31 11:46:18 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,18 +1,15 @@
 <?
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
-
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
- $cHeadres = array(
+$cHeadres = array(
       'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
       'Accept-Language: en-US,en;q=0.5',
       'Connection: Keep-Alive',
       'Pragma: no-cache',
       'Cache-Control: no-cache'
      );
-
-
      function dlPage($link) {
         global $cHeadres;
         $ch = curl_init();
@@ -34,19 +31,29 @@ require 'scraperwiki/simple_html_dom.php';
          return $dom;
         }
        }
-       
-for($page = 1; $page <3; $page++)
+for($page = 0; $page < 32; $page+=16)
 {
-    
- $link = 'https://e.fbr.gov.pk/Registration/searchDetail.aspx?crup='.$page;
- $maincode = dlPage($link); 
-$ntn = $maincode->find("//*[@id='lblSRNTN']",0)->plaintext;
- echo  "This is NTN number => $ntn \n";
-    
-    
-    
-    
-   // $NEWLINK    =   file_get_html($link);
-   // echo $NEWLINK;
+  $link = 'https://alumni.iba.edu.pk/alumni/findclassmate/a/'.$page;
+  $maincode = dlPage($link); 
+  echo $maincode;
 }
+//
+// // Read in a page
+// $html = scraperwiki::scrape("http://foo.com");
+//
+// // Find something on the page using css selectors
+// $dom = new simple_html_dom();
+// $dom->load($html);
+// print_r($dom->find("table.list"));
+//
+// // Write out to the sqlite database using scraperwiki library
+// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
+//
+// // An arbitrary query against the database
+// scraperwiki::select("* from data where 'name'='peter'")
+// You don't have to do things with the ScraperWiki library.
+// You can use whatever libraries you want: https://morph.io/documentation/php
+// All that matters is that your final data is written to an SQLite database
+// called "data.sqlite" in the current working directory which has at least a table
+// called "data".
 ?>
