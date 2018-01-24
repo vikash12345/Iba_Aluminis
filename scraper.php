7b56e3eb37eb4eb25e5585e2b7046ea6d9b5c226
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-01-24 13:27:10 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -14,7 +14,7 @@ if($html)
 {
   foreach($html->find("/html/body/div/div[2]/div[1]/div/div[2]/div[2]/div") as $element)
   {
-    $name  = $element->find("./div[1]/div/strong",0)->plaintext;
+    $name  = $element->find("./div/div/strong",0);
    echo "$name\n";
   }
 }
